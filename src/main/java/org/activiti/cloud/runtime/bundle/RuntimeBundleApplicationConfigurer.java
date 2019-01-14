f53b5df2c12dc42db10cda9411c3a372f0c04189
Igor Dianov <igor@dianov.com>
2019-01-13 17:30:00 -0800

    feat: add IRT Activity Workflow Process Implementation

--- src/main/java/org/activiti/cloud/runtime/bundle/RuntimeBundleApplicationConfigurer.java
+++ src/main/java/org/activiti/cloud/runtime/bundle/RuntimeBundleApplicationConfigurer.java
@@ -2,17 +2,42 @@ package org.activiti.cloud.runtime.bundle;
 
 import java.util.Arrays;
 
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.impl.rules.RulesDeployer;
+import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.spring.SpringProcessEngineConfiguration;
 import org.activiti.spring.boot.ProcessEngineConfigurationConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class RuntimeBundleApplicationConfigurer implements ProcessEngineConfigurationConfigurer {
+	
+	@Value("classpath:workflows/*.*")
+	private Resource[] deploymentResources;
+	
 	
 	@Override
 	public void configure(SpringProcessEngineConfiguration configuration) {
 		configuration.setCustomPostDeployers(Arrays.asList(new RulesDeployer()));
 	}
+	
+	@Bean
+	public CommandLineRunner deployMortgageQAResources(RepositoryService repositoryService) {
+		return (args) -> {
+	        DeploymentBuilder deployment = repositoryService.createDeployment()
+        	.name("IRT Process Workflow")
+            .enableDuplicateFiltering();
+	        
+	        for(Resource resource: deploymentResources)
+	        	deployment.addInputStream(resource.getFilename(), resource.getInputStream());
+	        
+            deployment.deploy();			
+		};
+	}
+	
 	
 }
--- src/test/java/org/activiti/cloud/runtime/bundle/RuntimeBundleApplicationIT.java
+++ src/test/java/org/activiti/cloud/runtime/bundle/RuntimeBundleApplicationIT.java
@@ -1,8 +1,17 @@
 package org.activiti.cloud.runtime.bundle;
 
-import org.activiti.cloud.runtime.bundle.RuntimeBundleApplication;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,8 +21,67 @@ import org.springframework.test.context.junit4.SpringRunner;
 @DirtiesContext
 public class RuntimeBundleApplicationIT {
 
+	@Autowired
+	private RuntimeService runtimeService;
+	
+	@Autowired
+	private TaskService taskService;
+
     @Test
     public void contextLoads() throws Exception {
+    	//
+    }
+    
+    @Test
+    public void startProcess() throws Exception {
+    	// given
+        Map<String, Object> variableMap = new HashMap<String, Object>();
+        variableMap.put("isHighRisk", false);
+        variableMap.put("isOverLimit", false);
+        
+        // when
+        ProcessInstance processInstance = runtimeService.createProcessInstanceBuilder()
+        		.processDefinitionKey("IRTProcessWorkflow")
+        		.variables(variableMap)
+        		.businessKey("businessKey")
+        		.start();
+        
+ 	    assertNotNull(processInstance);
+        
+        // Complete Due Diligence
+        Task task = taskService.createTaskQuery()
+        		   .processInstanceBusinessKey("businessKey")
+        		   .singleResult();
+        
+        assertNotNull(task);
+        
+        taskService.complete(task.getId());
 
+        // Key Directive  into iApprove
+        task = taskService.createTaskQuery()
+     		   .processInstanceBusinessKey("businessKey")
+     		   .singleResult();
+     
+	    assertNotNull(task);
+	     
+	    taskService.complete(task.getId());
+
+	    // Verify Information  Correct
+        task = taskService.createTaskQuery()
+      		   .processInstanceBusinessKey("businessKey")
+      		   .singleResult();
+      
+ 	    assertNotNull(task);
+ 	     
+ 	    taskService.complete(task.getId());
+	    
+        // then
+        ProcessInstance result = runtimeService.createProcessInstanceQuery()
+        		.processInstanceBusinessKey("businessKey")
+        		.singleResult();
+        
+        assertNotNull(result);
+
+ 	    
     }
 }
